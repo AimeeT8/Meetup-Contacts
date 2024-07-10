a884eb8c2f8adad8cb851c86a2fbbc6c0ba6fe92
aimee_t <aimeetemple14@gmail.com>
2024-07-10 19:38:14 -0400

    Changed the look of PhotosPicker in EditView

--- MyMeetupContacts/EditView.swift
+++ MyMeetupContacts/EditView.swift
@@ -44,30 +44,46 @@ struct EditView: View {
                      .textFieldStyle(.roundedBorder)
                      .padding()
                  
-                 
-                 ZStack {
-                     Rectangle()
-                         .fill(.secondary)
-                         .frame(width: 300, height: 300)
-                     Text("Tap to select photo")
-                         .foregroundStyle(.white)
-                         .font(.headline)
-                     image?
-                         .resizable()
-                         .scaledToFit()
-                         .frame(width: 300, height: 300)
-                 }
-                 .onTapGesture {
-                     showingImagePicker = true
-                 }
-             }
-             .onChange(of: inputUIImage) { _ in loadImage() }
-             .sheet(isPresented: $showingImagePicker) {
                  PhotosPicker(selection: $pickerItems) {
-                     Label("Select a Picture", systemImage: "photo")
+                     if let image {
+                         image
+                             .resizable()
+                             .scaledToFit()
+                     } else {
+                         ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                     }
                  }
-                     
+                 .buttonStyle(.plain)
+                 .onChange(of: pickerItems, loadImage)
                  
+                 
+                 
+                 
+                 //                 ZStack {
+                 //                     Rectangle()
+                 //                         .fill(.secondary)
+                 //                         .frame(width: 300, height: 300)
+                 //                     Text("Tap to select photo")
+                 //                         .foregroundStyle(.white)
+                 //                         .font(.headline)
+                 //                     image?
+                 //                         .resizable()
+                 //                         .scaledToFit()
+                 //                         .frame(width: 300, height: 300)
+                 //                 }
+                 //                 .onTapGesture {
+                 //                     showingImagePicker = true
+                 //                 }
+                 
+                 
+                 //             .onChange(of: inputUIImage) { _ in loadImage() }
+                 //             .sheet(isPresented: $showingImagePicker) {
+                 //                 PhotosPicker(selection: $pickerItems) {
+                 //                     Label("Select a Picture", systemImage: "photo")
+                 //                 }
+                 //
+                 //
+                 //             }
              }
              
              .toolbar {
@@ -93,10 +109,15 @@ struct EditView: View {
         
      }
     
+    
     func loadImage() {
-        guard let inputUIImage = inputUIImage else { return }
-        image = Image(uiImage: inputUIImage)
-        savingUIImage = inputUIImage
+        Task {
+            guard let imageData = try await pickerItems?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            let beginImage = CIImage(image: inputImage)
+            
+        }
     }
     
    
